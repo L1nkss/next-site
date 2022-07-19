03f86ddebd2f2cd9aaa3b44ff0c0ac23d5282127
Сергей <s.harlov@blanc.ru>
2022-07-19 21:13:22 +0500

    eslint

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { App } from "./app/App";
+import ReactDOM from "react-dom/client"
 
-const container = document.getElementById("root");
-if (container === null) throw new Error('Root container missing in index.html')
+import { App } from "./app/App"
 
-const root = ReactDOM.createRoot(container);
+const container = document.getElementById("root")
+if (container === null) throw new Error("Root container missing in index.html")
+
+const root = ReactDOM.createRoot(container)
 
 root.render(<App />)
