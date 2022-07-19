b3c2de5d466a7eedb20d73e3fbd223b22657e776
Сергей <s.harlov@blanc.ru>
2022-07-20 02:02:29 +0500

    alises

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import ReactDOM from "react-dom/client"
 
-import { App } from "./app/App"
-
+import { App } from "@app/App"
 import "./styles/index.scss"
 
 const container = document.getElementById("root")
