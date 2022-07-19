d27321335e53beea973b2d1d811968026fed4b18
Сергей <s.harlov@blanc.ru>
2022-07-19 20:34:29 +0500

    react setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,10 @@
-console.log('Hello world 4');
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./app/App";
+
+const container = document.getElementById("root");
+if (container === null) throw new Error('Root container missing in index.html')
+
+const root = ReactDOM.createRoot(container);
+
+root.render(<App />)
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,24 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+let mode = 'development';
+if (process.env.NODE_ENV === 'production') {
+  mode = 'production';
+}
 
 module.exports = {
     entry: './src/index.tsx',
-    mode: 'development',
+    mode,
     module: {
       rules: [
         {
-          test: /\.tsx?$/,
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            loader: "babel-loader",
+            options: { presets: ["@babel/env"] }
+        },
+        {
+          test: /\.(tsx|ts)?$/,
           use: 'ts-loader',
           exclude: /node_modules/,
         },
@@ -18,6 +29,7 @@ module.exports = {
           directory: path.join(__dirname, 'public'),
         },
         compress: true,
+        hot: true,
         port: 3000,
     },
     plugins: [
@@ -31,6 +43,7 @@ module.exports = {
     },
     output: {
       filename: 'bundle.js',
+      clean: true,
       path: path.resolve(__dirname, 'dist'),
     },
 }
